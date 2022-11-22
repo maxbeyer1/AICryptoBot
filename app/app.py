b83cdf447f068fabd1f3c5dab5101acd784e789f
maxbeyer1 <max.beyer09@gmail.com>
2022-11-22 11:39:43 -0500

    Added regression functions

--- app/app.py
+++ app/app.py
@@ -1,8 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
 
-data = pd.read_csv('./data/ETHUSDT_1h.csv', parse_dates=['time'],index_col='time')
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
+from sklearn.linear_model import BayesianRidge, ElasticNetCV
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+data = pd.read_csv('./data/ETHUSDT_1h.csv', parse_dates=['time'], index_col='time')
+
+print(data.columns.tolist())
+
+# data['time'] = pd.to_datetime(data['time'], unit='s')
+
 print(data.head())
+print(data.info())
 
 # close price graphs
 plt.figure(figsize=(15,8))
@@ -10,7 +23,7 @@ plt.figure(figsize=(15,8))
 
 plt.legend()
 plt.xlabel('time')
-# plt.show()
+plt.show()
 
 eth = data['close']
 
@@ -28,3 +41,60 @@ plt.title('5-Day Moving Average on Daily Closing Price')
 plt.xlabel('time')
 plt.ylabel('price in USD')
 plt.show()
+
+# calculate daily average price
+data['daily_avg'] = (data['open'] + data['high'] + data['low'] + data['close']) / 4
+data['daily_avg_After_Month']=data['daily_avg'].shift(-30)
+x_ETH = data.dropna().drop(['daily_avg_After_Month','daily_avg'], axis=1)
+y_ETH = data.dropna()['daily_avg_After_Month']
+x_train_ETH, x_test_ETH, y_train_ETH, y_test_ETH = train_test_split(x_ETH, y_ETH, test_size=0.2, random_state=43)
+x_forecast_ETH =  data.tail(30).drop(['daily_avg_After_Month','daily_avg'], axis=1)
+
+# define regression function
+def regression(X_train, X_test, y_train, y_test):
+    Regressor = {
+        'Random Forest Regressor': RandomForestRegressor(n_estimators=200),
+        'Gradient Boosting Regressor': GradientBoostingRegressor(n_estimators=500),
+        'ExtraTrees Regressor': ExtraTreesRegressor(n_estimators=500, min_samples_split=5),
+        'Bayesian Ridge': BayesianRidge(),
+        'Elastic Net CV': ElasticNetCV()
+    }
+
+    for name, clf in Regressor.items():
+        print(name)
+        clf.fit(X_train, y_train)
+    
+        print(f'R2: {r2_score(y_test, clf.predict(X_test)):.2f}')
+        print(f'MAE: {mean_absolute_error(y_test, clf.predict(X_test)):.2f}')
+        print(f'MSE: {mean_squared_error(y_test, clf.predict(X_test)):.2f}')
+        print()
+
+regression(x_train_ETH, x_test_ETH, y_train_ETH, y_test_ETH)
+
+# define prediction function
+def prediction(name, X, y, X_forecast):
+    model = ExtraTreesRegressor(n_estimators=500, min_samples_split=5)
+    model.fit(X, y)
+    target = model.predict(X_forecast)
+    return target
+
+# forecasted_ETH = prediction('ETH', x_ETH, y_ETH, x_forecast_ETH)
+
+# define index for next 30 days
+last_date=data.iloc[-1].name
+modified_date = last_date + dt.timedelta(days=1)
+new_date = pd.date_range(modified_date,periods=30,freq='D')
+
+forecasted_ETH = pd.DataFrame(forecasted_ETH, columns=['daily_avg'], index=new_date)
+
+ethereum = pd.concat([data[['daily_avg']], forecasted_ETH])
+
+plt.figure(figsize=(15,8))
+(ethereum[:-30]['daily_avg']).plot(label='Historical Price')
+(ethereum[-31:]['daily_avg']).plot(label='Predicted Price')
+
+plt.xlabel('Time')
+plt.ylabel('Price in USD')
+plt.title('Prediction on Daily Average Price of Ethereum')
+plt.legend()
+# plt.show()
