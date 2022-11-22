466b367143e4f9bdbb5054e6c4261541e724b2f5
maxbeyer1 <max.beyer09@gmail.com>
2022-11-22 09:15:16 -0500

    Plotted data

--- app/app.py
+++ app/app.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 data = pd.read_csv('./data/ETHUSDT_1h.csv', parse_dates=['time'],index_col='time')
 print(data.head())
+
+plt.figure(figsize=(15,8))
+(data['close']).plot(color='blue', label='close')
+
+plt.legend()
+plt.xlabel('time')
+plt.show()
