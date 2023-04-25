9646594dca259009b546258feeaacffc015750a8
maxbeyer1 <max.beyer09@gmail.com>
2023-04-25 11:37:41 -0400

    Updated RSI integrate

--- app/app.py
+++ app/app.py
@@ -66,6 +66,38 @@ data['daily_avg'] = (data['open'] + data['high'] + data['low'] + data['close']) 
 # add column with daily avg a month prior
 data['daily_avg_After_Month']=data['daily_avg'].shift(-168)
 
+
+
+def rsi(df, periods = 14, ema = True):
+    """
+    Returns a pd.Series with the relative strength index.
+    """
+    close_delta = df['close'].diff()
+
+    # Make two series: one for lower closes and one for higher closes
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+    
+    if ema == True:
+	    # Use exponential moving average
+        ma_up = up.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+        ma_down = down.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+    else:
+        # Use simple moving average
+        ma_up = up.rolling(window = periods, adjust=False).mean()
+        ma_down = down.rolling(window = periods, adjust=False).mean()
+        
+    rsi = ma_up / ma_down
+    rsi = 100 - (100/(1 + rsi))
+    return rsi
+
+rsi_ETH = []
+
+for i in range(14, data.index.stop):
+    rsi_ETH.append(rsi(data.iloc[i-14:i]))
+
+print(rsi_ETH)
+
 # drop rows that contain empty values 
 x_ETH = data.dropna().drop(['daily_avg_After_Month','daily_avg'], axis=1)
 y_ETH = data.dropna()['daily_avg_After_Month']
